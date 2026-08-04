f35b229ec4091db1b6891e084fbf5c72150d34e5
bowo rahayu <rahayuributwibowo@gmail.com>
2026-08-04 21:04:46 +0700

    Adjust hero padding and remove unused text

--- test-html.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-const html = fs.readFileSync('public/index.html', 'utf8');
-if (html.includes('cursor-pointer cursor-pointer')) {
-  console.log('Error: duplicate cursor-pointer');
-} else {
-  console.log('HTML is clean');
-}
